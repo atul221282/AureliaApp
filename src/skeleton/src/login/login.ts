6015505e44f20385fe7d9de8729328a13d9f5e1e
atul <atul@ISHANA>
2016-07-16 18:41:42 +0930

    Add project files.

---
+++ src/skeleton/src/login/login.ts
@@ -0,0 +1,11 @@
+export class Login {
+
+    name: string;
+    password: string;
+
+    constructor() {
+        this.name = "atul221282@gmail.com";
+        this.password = "123456";
+    }
+    
+}
